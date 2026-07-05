9e87e4c721ce408101250363c2a89f0c004f02a8
Tofu <102429571+TrieuPhu03@users.noreply.github.com>
2026-07-06 03:23:08 +0700

    Revert "Feat/unit test"

--- front_end/src/constants/app.ts
+++
@@ -1,8 +0,0 @@
-export const APP_NAME = "SRT Todo";
-
-export const SUPPORTED_LANGUAGES = [
-  { code: "en", label: "English" },
-  { code: "vi", label: "Tieng Viet" },
-] as const;
-
-export type SupportedLanguage = (typeof SUPPORTED_LANGUAGES)[number]["code"];
